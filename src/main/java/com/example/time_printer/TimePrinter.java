cc3c4f9d521e543bb61ce0ac9e3b14d92cc3446f
Vikram Krishna A <VikramKrishna.A@tatacommunications.com>
2025-03-27 11:04:50 +0530

    Added Logger

--- src/main/java/com/example/time_printer/TimePrinter.java
+++ src/main/java/com/example/time_printer/TimePrinter.java
@@ -1,16 +1,20 @@
 package com.example.time_printer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.time.LocalTime;
 
 public class TimePrinter {
+    private static final Logger logger = LoggerFactory.getLogger(TimePrinter.class);
+
     public void startPrinting() {
         while (true) {
-            System.out.println("Current Time: " + LocalTime.now());
+            logger.info("Current Time: {}", LocalTime.now());
             try {
                 Thread.sleep(1000); // Sleep for 1 second
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
-                System.out.println("Thread interrupted");
+                logger.error("Thread interrupted", e);
                 break;
             }
         }
--- src/main/java/com/example/time_printer/TimePrinterApplication.java
+++ src/main/java/com/example/time_printer/TimePrinterApplication.java
@@ -3,9 +3,13 @@ package com.example.time_printer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class TimePrinterApplication implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(TimePrinterApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimePrinterApplication.class, args);
